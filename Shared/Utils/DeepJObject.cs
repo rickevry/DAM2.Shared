1ade1e0a46bdf8e30dec65d90cfc643623ceeb2f
Rickard Nilsson <rickard@rara.nu>
2021-02-26 11:48:03 +0100

    enable SourceLink

--- Shared/Utils/DeepJObject.cs
+++ Shared/Utils/DeepJObject.cs
@@ -19,7 +19,6 @@ namespace DAM2.Core.Actors.Shared.Utils
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
-                int a = 1;
             }
             return "";
         }
@@ -36,7 +35,6 @@ namespace DAM2.Core.Actors.Shared.Utils
             } catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
-                int a = 1;
             }
         }
 
