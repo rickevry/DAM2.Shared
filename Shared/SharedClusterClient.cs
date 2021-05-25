316355aeeb36421b8ebcd6aeef42a454e0008aac
daniel.klingstedt@tietoevry.com <daniel.klingstedt@tietoevry.com>
2021-05-25 16:12:25 +0200

    SAVEPOINT

--- Shared/SharedClusterClient.cs
+++ Shared/SharedClusterClient.cs
@@ -7,6 +7,7 @@ using Proto.Cluster;
 using Proto.Remote.GrpcCore;
 using DAM2.Core.Shared.Interface;
 using DAM2.Core.Shared.Settings;
+using System.Collections.Generic;
 
 namespace DAM2.Core.Shared
 {
@@ -18,6 +19,7 @@ namespace DAM2.Core.Shared
         private Cluster _cluster;
         private bool _cluster_ready = false;
         private readonly ISharedClusterProviderFactory _clusterProvider;
+        private Dictionary<string, int> retries = new();
 
         public SharedClusterClient(ILogger<SharedClusterClient> logger, IDescriptorProvider descriptorProvider, IClusterSettings clusterSettings, ISharedClusterProviderFactory clusterProvider)
         {
@@ -29,6 +31,7 @@ namespace DAM2.Core.Shared
 
         public async Task<T> RequestAsync<T>(string actorPath, string clusterKind,  object cmd)
         {
+            string key = $"{actorPath}_{clusterKind}";
             int counter = 0;
             while (!_cluster_ready && counter < 40)
             {
@@ -41,17 +44,30 @@ namespace DAM2.Core.Shared
                 var tokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(30));
                 var res = await _cluster.RequestAsync<T>(actorPath, clusterKind, cmd, tokenSource.Token)
                     .ConfigureAwait(false);
+
+                retries.Remove(key);
+                
                 return res;
             }
             catch (TimeoutException)
             {
+                if (retries.TryGetValue(key, out int value))
+                {
+                    Interlocked.Increment(ref value);
+                }
+
+                if (value > 5)
+                {
+                    _logger.LogError("Request timeout for {Id}", actorPath);
+                    return default(T);
+                }
 	            _logger.LogInformation("Recreate cluster because of timeout...");
                 await this.CreateCluster();
                 return await RequestAsync<T>(actorPath, clusterKind, cmd);
             }
             catch (Exception x)
             {
-                _logger.LogError(x, "Request timeout for {Id}", actorPath);
+                _logger.LogError(x, "Failed Request {Id}", actorPath);
                 return default(T);
             }
         }
